ed10c250fb8ceacb765a1369ce8ff3fbb1ab9224
Renee Nordholm <reneenordholm@gmail.com>
2019-05-29 00:58:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,9 +14,15 @@ def create_project_hash
   
   projects = {}
   
-  kickstarter.css("li.project.grid_4").each { |project| 
+  kickstarter.css("li.project.grid_4").each { 
+    |project| 
     title = project.css("h2.bbcard_name strong a").text
-    pro
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
   }
   
   projects
